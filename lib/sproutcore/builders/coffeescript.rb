ac388e888a2ff5237f34b92430bb34b9a56f0ceb
Vladimir Bobes Tuzinsky <vladimir.tuzinsky@gmail.com>
2011-05-10 21:28:47 +1000

    Simplified CoffeeScript builder, which now reuses all functionality of JavaScript builder

--- lib/sproutcore/builders/coffeescript.rb
+++ lib/sproutcore/builders/coffeescript.rb
@@ -6,18 +6,15 @@ module SC
       new(entry).build(dst_path)
     end
 
-    def build(dst_path)
+    def readlines(src_path)
+      ";" + ::CoffeeScript.compile(super)
+    rescue NameError
       begin
         require "coffee-script"
+        retry
       rescue LoadError => e
         raise "Cannot compile #{entry.source_path} because coffeescript is not installed. Please install coffeescript to continue."
       end
-
-      coffee = readlines(entry.source_path)
-      js = ::CoffeeScript.compile(coffee)
-      js = rewrite_inline_code(js)
-      js = handle_debug_code(js)
-      writeline dst_path, ";#{js}"
     end
   end
 end
